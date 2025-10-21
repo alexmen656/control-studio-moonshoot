ec8d5e602ee281cd1097355811439a137a173c7b
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-21 07:25:07 +0200

    feat: Add new items to "New" in sidebar

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -30,8 +30,33 @@
                             clip-rule="evenodd" />
                     </svg>
                     <div>
-                        <div class="text-sm font-medium text-gray-900 dark:text-gray-100">Video upload</div>
+                        <div class="text-sm font-medium text-gray-900 dark:text-gray-100">Longformat Video upload</div>
                         <div class="text-xs text-gray-500 dark:text-gray-400">Upload video files from your device</div>
+                    </div>
+                </button>
+                <button @click="triggerVideoUpload"
+                    class="w-full text-left px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center space-x-3 transition-colors">
+                    <svg class="w-5 h-5 text-gray-600 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                            d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <div>
+                        <div class="text-sm font-medium text-gray-900 dark:text-gray-100">Shortformat Video upload</div>
+                        <div class="text-xs text-gray-500 dark:text-gray-400">Upload video files from your device</div>
+                    </div>
+                </button>
+                <button @click="triggerVideoUpload"
+                    class="w-full text-left px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 flex items-center space-x-3 transition-colors">
+                    <svg class="w-5 h-5 text-gray-600 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                            d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <div>
+                        <div class="text-sm font-medium text-gray-900 dark:text-gray-100">Create Post</div>
+                        <div class="text-xs text-gray-500 dark:text-gray-400">Create a post with our post creation tool
+                        </div>
                     </div>
                 </button>
             </div>
@@ -87,7 +112,7 @@
                 <div class="space-y-2">
                     <div class="flex items-center justify-between text-sm">
                         <span class="text-gray-600 dark:text-gray-400">{{ storageUsed }} {{ unit }} of {{ storageTotal
-                            }}
+                        }}
                             GB
                             used</span>
                     </div>
