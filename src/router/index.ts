88dec4b6332f7e7dce6b7afd57a3802b3242c1eb
Alex <133679349+alexmen656@users.noreply.github.com>
2025-10-23 07:20:36 +0200

    feat: Add routes and LandingPage file

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LandingPage from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'landing',
+      component: LandingPage,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('../views/SignUpView.vue'),
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
