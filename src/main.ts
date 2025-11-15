7bf6d79d50f20bd6e02bcf823da4236d158b81b9
Alex <133679349+alexmen656@users.noreply.github.com>
2025-11-15 17:39:02 +0100

    fix: Add Google client id for Google login/signup

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import axios from './axios.ts'
+import GoogleSignInPlugin from 'vue3-google-signin'
 
 const app = createApp(App)
+
+app.use(GoogleSignInPlugin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+})
 
 app.use(createPinia())
 app.use(router)
