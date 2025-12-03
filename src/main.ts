0214194b338630b2c49ab75579691afa9c44e4fe
Alex <133679349+alexmen656@users.noreply.github.com>
2025-12-04 00:13:06 +0100

    feat: Move Google client id from .env to main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import GoogleSignInPlugin from 'vue3-google-signin'
 const app = createApp(App)
 
 app.use(GoogleSignInPlugin, {
-  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  clientId: '706582238302-k3e6bqv81en6u97gf8l5pq883p773236.apps.googleusercontent.com', //import.meta.env.VITE_GOOGLE_CLIENT_ID,
 })
 
 app.use(createPinia())
